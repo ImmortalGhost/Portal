3b2b7add27bf85a1ad4640fb0650997bd0db126e
MaksimArtemuk <maksimartemuk1992@gmail.com>
2016-05-01 20:14:31 +0300

    rm1

---
+++ core/src/main/java/ru/epam/unversity_portal/core/dao/App.java
@@ -0,0 +1,10 @@
+package ru.epam.unversity_portal.core.dao;
+
+/**
+ * Created by maksim on 01.05.16.
+ */
+public class App {
+    public  static void main(String []argc){
+        System.out.print("Hello");
+    }
+}
